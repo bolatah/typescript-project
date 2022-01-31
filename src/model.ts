0c383bcd70380639ef69482543d304b601bdc789
bolatah <bolatah@outlook.com>
2022-01-31 20:57:53 +0100

    drag & drop

--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,22 @@
     todo:string;
     isDone: boolean;
 }
+
+// type Actions = 
+// | {type: "add"; payload: string }
+// | {type: "remove"; payload: number }
+// | {type: "done"; payload: number }; 
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+
+// };
+
+// import { useReducer } from "react";
+
+// const ReducerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, []);
+  
+//     return <div />;
+
+// };
+ 
